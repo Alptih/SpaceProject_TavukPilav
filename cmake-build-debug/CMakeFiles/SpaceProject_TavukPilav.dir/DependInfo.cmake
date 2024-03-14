a91d91021514575753eadc35663ba6ab185889ad
Utku-tan <83100541+Utku-tan@users.noreply.github.com>
2024-03-14 22:45:41 +0300

    U.0.5 //Bazı hatalar giderildi

---
+++ cmake-build-debug/CMakeFiles/SpaceProject_TavukPilav.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/main.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/main.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/main.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Event/AbandonedPlanetEvent.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AbandonedPlanetEvent.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AbandonedPlanetEvent.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Event/AsteroidBeltEvent.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AsteroidBeltEvent.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AsteroidBeltEvent.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Event/SpaceEventManager.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpaceEventManager.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpaceEventManager.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Event/SpacePiratesEvent.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpacePiratesEvent.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpacePiratesEvent.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Ship/CosmicWind.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/CosmicWind.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/CosmicWind.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Ship/RockBreaker.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/RockBreaker.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/RockBreaker.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Ship/SpaceShip.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpaceShip.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpaceShip.cpp.obj.d"
+  "C:/Users/utkuy/CLionProjects/SpaceProject_TavukPilav/src/Ship/SpeedOfLight.cpp" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpeedOfLight.cpp.obj" "gcc" "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpeedOfLight.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SpaceProject_TavukPilav.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceProject_TavukPilav.dir/main.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/main.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AbandonedPlanetEvent.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AbandonedPlanetEvent.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AsteroidBeltEvent.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/AsteroidBeltEvent.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpaceEventManager.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpaceEventManager.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpacePiratesEvent.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Event/SpacePiratesEvent.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/CosmicWind.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/CosmicWind.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/RockBreaker.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/RockBreaker.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpaceShip.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpaceShip.cpp.obj.d"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpeedOfLight.cpp.obj"
+  "CMakeFiles/SpaceProject_TavukPilav.dir/src/Ship/SpeedOfLight.cpp.obj.d"
+  "SpaceProject_TavukPilav.exe"
+  "SpaceProject_TavukPilav.exe.manifest"
+  "SpaceProject_TavukPilav.pdb"
+  "libSpaceProject_TavukPilav.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceProject_TavukPilav.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
